04fc822feb94c5ad54cecedecbcb1c3b122603d0
leaf-kit <272545784+leaf-kit@users.noreply.github.com>
2026-04-11 08:57:09 +0900

    feat: update leaf-kit-tour formula to v0.3.0

--- Formula/leaf-kit-tour.rb
+++ Formula/leaf-kit-tour.rb
@@ -1,12 +1,12 @@
 class LeafKitTour < Formula
   desc "Interactive CLI installer for leaf-kit Markdown toolkit"
   homepage "https://github.com/leaf-kit/leaf-kit-tour"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
-    url "https://github.com/leaf-kit/leaf-kit-tour/releases/download/v0.2.0/leaf-kit-tour-0.2.0-darwin-x86_64.tar.gz"
-    sha256 "41371ccfff7726bfd1275e0c3faef53fe4fb07464df242a4c5153406376f06c3"
+    url "https://github.com/leaf-kit/leaf-kit-tour/releases/download/v0.3.0/leaf-kit-tour-0.3.0-darwin-x86_64.tar.gz"
+    sha256 "b864178af6a14d1c86691663ab1623f6148c9343cf19950acd25698eec707686"
   end
 
   def install
