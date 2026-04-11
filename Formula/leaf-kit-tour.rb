6388905c53a0240ebb7e61021856b0ec85d49a73
Leaf.md <nlfactory@naver.com>
2026-04-11 11:27:48 +0900

    chore: bump leaf-kit-tour formula to v0.4.0

--- Formula/leaf-kit-tour.rb
+++ Formula/leaf-kit-tour.rb
@@ -1,12 +1,12 @@
 class LeafKitTour < Formula
   desc "Interactive CLI installer for leaf-kit Markdown toolkit"
   homepage "https://github.com/leaf-kit/leaf-kit-tour"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
-    url "https://github.com/leaf-kit/leaf-kit-tour/releases/download/v0.3.0/leaf-kit-tour-0.3.0-darwin-x86_64.tar.gz"
-    sha256 "b864178af6a14d1c86691663ab1623f6148c9343cf19950acd25698eec707686"
+    url "https://github.com/leaf-kit/leaf-kit-tour/releases/download/v0.4.0/leaf-kit-tour-0.4.0-darwin-x86_64.tar.gz"
+    sha256 "bf2b7d80d0a4e2b020077b32c94aaf37fbc963bed446e92cc55b52ea39acafe0"
   end
 
   def install
