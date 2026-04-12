a21aea75d9e522e01dd1c9f7321cfb4b5073a86e
leaf-kit <272545784+leaf-kit@users.noreply.github.com>
2026-04-12 09:03:39 +0900

    chore: update formula to v0.4.4

--- Formula/leaf-kit-tour.rb
+++ Formula/leaf-kit-tour.rb
@@ -1,8 +1,8 @@
 class LeafKitTour < Formula
   desc "Interactive CLI installer for leaf-kit Markdown toolkit"
   homepage "https://github.com/leaf-kit/leaf-kit-tour"
-  url "https://github.com/leaf-kit/leaf-kit-tour/archive/refs/tags/v0.4.3.tar.gz"
-  sha256 "edca3bf3bbe3f8c0b1302f1c3b9be9080c05b54a06e00b3b4c1527280723937e"
+  url "https://github.com/leaf-kit/leaf-kit-tour/archive/refs/tags/v0.4.4.tar.gz"
+  sha256 "abddb4df5bce7aa2792ae2b18047e33e5fce569037de1a837874a491fb8cdf22"
   license "MIT"
 
   depends_on "rust" => :build
